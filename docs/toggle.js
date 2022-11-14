846e1040275d9a01cd11b2997962eeff8ca48fb3
Yuying-Jin <yxj5181@psu.edu>
2022-11-13 22:25:28 -0500

    add select all and reset buttons

--- docs/toggle.js
+++ docs/toggle.js
@@ -1,8 +1,15 @@
 function init() {
-    var fieldset = document.getElementsByTagName('input');
-    for (var i = 0; i < fieldset.length; i++) {
-        fieldset[i].addEventListener('click', toggle, false);
+    var inputs = document.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('click', toggle, false);
     }
+
+    var buttons = document.getElementsByTagName("button")
+    buttons[0].addEventListener('click', selectAll,false)
+    buttons[1].addEventListener('click', reset,false)
+
+    var fieldset = document.getElementsByTagName("fieldset")[0]
+    fieldset.addEventListener('mousedown', mouseDown, false)
 }
 
 function toggle() {
@@ -38,5 +45,28 @@ function toggle() {
         break;
     }
 }
+
+function selectAll(){
+    var objects = document.getElementsByTagName("span")
+    for (var i = 0; i < objects.length; i++) {
+        objects[i].classList.add("on")
+    }
+    var classList = document.getElementsByTagName("input");
+    for (var i = 0; i < classList.length; i++){
+        classList[i].checked = true;
+    }
+}
+
+function reset(){
+    var objects = document.getElementsByTagName("span")
+    for (var i = 0; i < objects.length; i++) {
+        objects[i].classList.remove("on")
+    }
+    var classList = document.getElementsByTagName("input");
+    for (var i = 0; i < classList.length; i++){
+        classList[i].checked = false;
+    }
+}
+
 
 window.onload = init;
